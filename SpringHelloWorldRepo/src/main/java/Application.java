9421706f7d9a252e07f078d56ecf45e92b773094
rachelxueru <rachelxueru@gmail.com>
2021-02-13 15:23:10 -0800

    this is rachel's second commit

--- SpringHelloWorldRepo/src/main/java/Application.java
+++ SpringHelloWorldRepo/src/main/java/Application.java
@@ -1,6 +1,6 @@
 import com.leapcode.service.SpeakerService;
 import com.leapcode.service.SpeakerServiceImpl;
-
+// this is Rachel's second command
 public class Application {
     public static void main(String[] args){
         SpeakerService service = new SpeakerServiceImpl();
