2483cfb8f4d3f61259bb94f6d09a1626f823ae4a
xueru rong <37965833+rachelxueru@users.noreply.github.com>
2021-02-13 15:13:30 -0800

    Add files via upload

---
+++ SpringHelloWorldRepo/src/main/java/com/leapcode/model/Speaker.java
@@ -0,0 +1,23 @@
+package com.leapcode.model;
+
+// This is Rachel's comment
+public class Speaker {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
